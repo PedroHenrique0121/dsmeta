0f3d438a71f31ff15a422ac01c1c11a2f119be94
PedroHenrique0121 <pedrohenriquegd2016@gmail.com>
2022-07-16 08:47:20 -0300

    Sale Listing

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -6,15 +6,19 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./style.css"
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sale";
 function SalesCard() {
     const max = new Date();
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    const [sales, setSales] = useState<Sale[]>([]);
     useEffect(() => {
-        axios.get("http://localhost:8080/sales")
-            .then(Response=> console.log(Response.data))
+        axios.get(`${BASE_URL}/sales`)
+            .then(response =>
+                setSales(response.data.content))
     }, [])
 
 
@@ -56,39 +60,28 @@ function SalesCard() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotiFicationButton></NotiFicationButton>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotiFicationButton></NotiFicationButton>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <NotiFicationButton></NotiFicationButton>
-                                </td>
-                            </tr>
+
+                            {
+                                sales.map(sale => {
+
+                                    return (
+                                        <tr key={sale.id}>
+                                            <td className="show992">{sale.id}</td>
+                                            <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                            <td>{sale.sellerName}</td>
+                                            <td className="show992">{sale.visited}</td>
+                                            <td className="show992">{sale.deals}</td>
+                                            <td>R$ {sale.amount.toFixed(2)}</td>
+                                            <td>
+                                                <NotiFicationButton></NotiFicationButton>
+                                            </td>
+                                        </tr>
+                                    )
+
+                                })
+                            }
+
+
                         </tbody>
 
                     </table>
